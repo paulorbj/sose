1f80783485523cec3b5a7100c3c3faec1e8bafd2
shaman989 <shaman989@gmail.com>
2015-06-27 15:57:41 +0000

    versao mais nova,,,nao ta pronto

--- src/br/com/sose/daoImpl/externo/ExternoDao.java
+++ src/br/com/sose/daoImpl/externo/ExternoDao.java
@@ -9,6 +9,15 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceException;
 import org.hibernate.Query;
 
+import java.util.List;
+
+import org.hibernate.Query;
+import org.springframework.stereotype.Repository;
+
+import br.com.sose.daoImpl.HibernateDaoGenerico;
+import br.com.sose.entity.admistrativo.Unidade;
+import br.com.sose.entity.orcamento.Orcamento;
+import br.com.sose.to.OrcRepGenericoTO;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.jpa.JpaCallback;
@@ -28,60 +37,66 @@ public class ExternoDao extends HibernateDaoGenerico<Externo, Long> {
 	}
 
 	@SuppressWarnings("unchecked")
-	public List<Externo> listarExternos(final String status) {
+	public List<Externo> listarExternos(final Long id) {
+				Query q = sessionFactory.getCurrentSession().createQuery("SELECT ext FROM "+ entityClass.getName() + " ext WHERE (h.ordemServico.unidade  = :unidade)");
+						
+//						
+//						
+//						("SELECT ext FROM "+ entityClass.getName() + " orc " +
+//						"LEFT JOIN FETCH orc.ordemServico os " +
+//						"LEFT JOIN FETCH os.notaFiscal nf " +
+//						"LEFT JOIN FETCH os.unidade u " +
+//						"LEFT JOIN FETCH u.fabricante f " +
+//						"LEFT JOIN FETCH u.equipamento e " +
+//						"LEFT JOIN FETCH u.laboratorio l " +
+//						"LEFT JOIN FETCH u.prestadorServicoExterno pse " +
+//						"LEFT JOIN FETCH orc.tecnicoResponsavel tr " +
+//						"LEFT JOIN FETCH tr.perfil p " +
+//						"LEFT JOIN FETCH orc.listaRequisicao lr " +
+//						"LEFT JOIN FETCH orc.listaAtividade la " +
+//						"LEFT JOIN FETCH orc.listaComponente lc " +
+//						"LEFT JOIN FETCH orc.listaDefeito ld WHERE orc.id=:id");
+				q.setParameter("id", id);
+				return q.list();
+			}
 
+			@SuppressWarnings("unchecked")
+			public List<OrcRepGenericoTO> listarOrcamentoOtimizado() {
+				Query q = sessionFactory.getCurrentSession().createQuery("SELECT NEW br.com.sose.to.OrcRepGenericoTO('Orçamento'," +
+						"h.statusString, t.usuario, os.numeroOrdemServico, ospai.numeroOrdemServico, " +
+						"c.nomeSistema, u.nome, nf.numero, os.serieFabricante, os.serieCliente,l.nome," +
+						"h.prioridade, nf.dataChegada, h.dataEntrada, h.laudoTecnicoAprovado,h.laudoTecnicoReprovado," +
+						"h.propostaReprovada,h.rejeitadoPeloLider,h.id,t.id,h.dataLimite,c.prazoDevolucao,os.bloqueado,os.caseAvaya,os.clienteAvaya,h.condicao, l.id, h.componentePendente)  FROM "+ entityClass.getName() + " h " +
+						"LEFT JOIN  h.tecnicoResponsavel t " +
+						"LEFT JOIN  h.ordemServico os " +
+						"LEFT JOIN  os.cliente c " +
+						"LEFT JOIN  os.unidadePai ospai " +
+						"LEFT JOIN  os.unidade u " +
+						"LEFT JOIN  u.laboratorio l " +
+						"LEFT JOIN  os.notaFiscal nf " +
+						"WHERE h.statusString != 'Finalizado' ORDER BY h.dataEntrada  ");
+				return q.list();
+			}
 
-		StringBuilder sb = new StringBuilder("SELECT NEW br.com.sose.entity.laudoTecnico.LaudoTecnico(" +
-				"h.id," +
-				"h.controle," +
-				"h.statusString," +
-				"os.numeroOrdemServico," +
-				"up.numeroOrdemServico," +
-				"c.nomeSistema," +
-				"un.nome," +
-				"os.serieFabricante," +
-				"os.serieCliente," +
-				"l.nome," +
-				"u.usuario) FROM "+ entityClass.getName() + " h " +
-				"LEFT JOIN h.ordemServico os " +
-				"LEFT JOIN os.unidadePai up " +
-				"LEFT JOIN os.cliente c " +
-				"LEFT JOIN os.unidade un " +
-				"LEFT JOIN un.laboratorio l " +
-				"LEFT JOIN h.criadoPor u " +
-				"WHERE h.statusString = :status ORDER BY h.dataCriacao DESC");
-		Query query = sessionFactory.getCurrentSession().createQuery(sb.toString());
-		query.setParameter("status", status);
-		return query.list();
+			public List<Orcamento> listarHistoricoReparo(final Unidade unidade, final String serieFabricante) {
+				StringBuilder sb = new StringBuilder("SELECT h FROM "+ entityClass.getName() + " h WHERE (h.ordemServico.unidade  = :unidade)");
+				if(serieFabricante != null){
+					sb.append(" AND (h.ordemServico.serieFabricante LIKE :paramSerieFabricante) ");
+				}
+				//				if(serieCliente != null){
+				//					sb.append(" AND (h.ordemServico.serieCliente LIKE :paramSerieCliente) ");
+				//				}
 
-	}
+				Query query = sessionFactory.getCurrentSession().createQuery(sb.toString());
 
-//	@SuppressWarnings("unchecked")
-//	public LaudoTecnico buscarPorId(final Long id) {		
-//		Query q = sessionFactory.getCurrentSession().createQuery("SELECT h FROM "+ entityClass.getName() + " h " +
-//				"LEFT JOIN FETCH h.ordemServico os " +
-//				"LEFT JOIN FETCH os.placasFilhas pf " +
-//				"LEFT JOIN FETCH pf.orcamento pfOrc " +
-//				"LEFT JOIN FETCH pfOrc.listaDefeito  " +
-//				"LEFT JOIN FETCH pf.reparo pfRep " +
-//				"LEFT JOIN FETCH pfRep.listaDefeito  " +
-//				"LEFT JOIN FETCH h.orcamento orc " +
-//				"LEFT JOIN FETCH orc.listaDefeito  " +
-//				"LEFT JOIN FETCH h.reparo rep " +
-//				"LEFT JOIN FETCH rep.listaDefeito WHERE h.id=:id");
-//		q.setParameter("id", id);
-//		LaudoTecnico r = (LaudoTecnico)q.uniqueResult();
-//		return r;
-//	}
-//
-//	@SuppressWarnings("unchecked")
-//	public LaudoTecnico buscarUltimoLaudoPorOrdemServico(final Long id) {		
-//		Query q = sessionFactory.getCurrentSession().createQuery("SELECT h FROM "+ entityClass.getName() + " h " +
-//				"WHERE h.controle = (SELECT MAX(x.controle) FROM LaudoTecnico x where x.ordemServico.id=:id)");
-//		q.setParameter("id", id);
-//		LaudoTecnico r = (LaudoTecnico)q.uniqueResult();
-//		return r;
-//	}
+				query.setParameter("unidade", unidade);
+				if(serieFabricante != null){
+					query.setParameter("paramSerieFabricante", serieFabricante);				}
+				//				if(serieCliente != null){
+				//					query.setParameter("paramSerieCliente", serieCliente);				}
 
+				return query.list();
 
-}
+			}
+
+		}
