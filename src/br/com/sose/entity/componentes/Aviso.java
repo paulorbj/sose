9fb72003a3a36f560f4fc3fdbb4fd34913e8606d
shaman989 <shaman989@gmail.com>
2015-08-12 16:44:28 +0000

    o ID nao é mais auto gerado, para poder funcionar o application listener que fiz... preciso acertar isso depois

--- src/br/com/sose/entity/componentes/Aviso.java
+++ src/br/com/sose/entity/componentes/Aviso.java
@@ -28,7 +28,7 @@ public class Aviso implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(length = 3100)
